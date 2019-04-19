226038ff3d2ddb27f0faaabafe046a013463e133
Artem Cherepnin <artnerab@gmail.com>
2019-04-19 08:43:06 +0300

    Правки по обязательному заданию

--- Lesson_1/easy/js/script.js
+++ Lesson_1/easy/js/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
 let money = prompt("Ваш бюджет на месяц?", ""),
-  time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-let appData = {
+  time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+  c1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+  h1 = prompt("Во сколько обойдется?", ""),
+  c2 = prompt("Введите необязательную статью расходов в этом месяце", ""),
+  h2 = prompt("Во сколько обойдется?", ""),
+  appData = {
   budget: money,
   timeData: time,
   expenses: {},
@@ -12,9 +15,7 @@ let appData = {
   savings: false
 };
 
-let costs = prompt("Введите обязательную статью расходов в этом месяце", ""),
-  howMuch = prompt("Во сколько обойдется?", "");
-
-appData.expenses[costs] = howMuch;
+appData.expenses[c1] = h1;
+appData.expenses[c2] = h2;
 
 alert("Ваш бюджет на один день составляет: " + money/30 + " рублей");
